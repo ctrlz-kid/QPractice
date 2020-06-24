901563ea69b4a405d3701c409ca30df654a4f761
ctrlz <3034589+ctrlz-kid@users.noreply.github.com>
2020-06-25 02:32:08 +0800

    静态变量实现单件模式；获得char指针（buffer）的内存地址。

--- src/GuiCore/mainfrom.cpp
+++ src/GuiCore/mainfrom.cpp
@@ -1,11 +1,52 @@
-#include "mainfrom.h"
+﻿#include "mainfrom.h"
 #include "ui_mainfrom.h"
+#include <QDebug>
+
+
+// 静态单件方式
+class ClsA
+{
+public:
+    int x;
+
+    static ClsA* instance();
+};
+
+ClsA* ClsA::instance()
+{
+    static ClsA a;
+    return &a;
+}
+
 
 MainFrom::MainFrom(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::MainFrom)
 {
     ui->setupUi(this);
+
+
+
+    char ch[] = "123456";
+
+    char* p = ch;
+
+    char** pp = &p;
+
+    qDebug() << ch;
+    qDebug() << p;
+
+    // 输出buffer的内存位置
+    qDebug() << QString("0x%1").arg((long long)*pp, 0, 16);
+
+
+    ClsA* a = ClsA::instance();
+    a->x = 1;
+
+    ClsA* b = ClsA::instance();
+    int y = a->x;
+    a->x = 2;
+
 }
 
 MainFrom::~MainFrom()
